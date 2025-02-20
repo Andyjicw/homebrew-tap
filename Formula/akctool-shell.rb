4fc1e310245d97210715d8bb44ed11538bcfe0f4
anker <leo.ji@anker-in.com>
2025-02-20 16:22:50 +0800

    add file

---
+++ Formula/akctool-shell.rb
@@ -0,0 +1,14 @@
+class AkcToolShell < Formula
+  desc "A simple and powerful shell script for automation"
+  homepage "https://github.com/Andyjicw/akcTool"
+  url "https://github.com/Andyjicw/akcTool/archive/v1.0.0.tar.gz"
+  sha256 "6ae18846feadd4e4e2bc696545a8c46db083b4503ad617332cd449ccdf1ff13f"
+  
+  def install
+    bin.install "akcTool" => "akcTool"
+  end
+  
+  test do
+    system "#{bin}/akcTool", "--version"
+  end
+end
